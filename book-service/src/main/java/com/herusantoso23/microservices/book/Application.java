efe80b77b391fcc4a27ada12f017a4a1df83a99e
Heru Santoso <herusantoso008@gmail.com>
2022-09-27 19:08:43 +0700

    add jenkins file and remove unit tests

--- book-service/src/main/java/com/herusantoso23/microservices/book/Application.java
+++ book-service/src/main/java/com/herusantoso23/microservices/book/Application.java
@@ -2,9 +2,9 @@ package com.herusantoso23.microservices.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
